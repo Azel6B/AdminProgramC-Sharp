88e4ba42e01678f57523ca18674699192e3640eb
Azel6B <aslandeleeuw666@gmail.com>
2026-02-16 12:36:14 +0100

    Added User.cs and Adjusted Program.cs
    Not yet finished

--- Admin Program C#/Program.cs
+++ Admin Program C#/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,14 @@ namespace Admin_Program_C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wekker");
+             // Create a new user
+            User myUser = new User("Admin", "Kalebaan 76", "adadadadad@gmail.com", "06 17385904", 27);
+            Console.WriteLine($"Created user: {myUser.Name}");
+            Console.WriteLine($"Created Email: {myUser.Email}");
+            Console.WriteLine($"Created Phone: {myUser.Phone}");
+            Console.WriteLine($"Created Address: {myUser.Address}");
+            Console.WriteLine($"Created Age: {myUser.Age}");
+            Console.WriteLine($"User Info: {myUser.GetUserInfo()}");
         }
     }
 }
---
+++ Admin Program C#/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Admin_Program_C_
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, string address, string email, string phone, int age)
+        {
+            Name = name;
+            Address = address;
+            Email = email;
+            Phone = phone;
+            Age = age;
+        }
+
+        public void ChangeEmail(string newEmail)
+        {
+            Email = newEmail;
+        }
+        public string GetUserInfo()
+        {
+            return $"Name: {Name}, Address: {Address}, Email: {Email}, Phone: {Phone}, Age: {Age}";
+        }
+    }
+}
